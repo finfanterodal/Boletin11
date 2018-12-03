603972d6c4f0a94107c90a7ec8af0876cf4f2a2f
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2018-12-03 11:26:45 +0100

    Boletin11_v2

--- Boletin11/src/boletin11/Intervalo.java
+++ Boletin11/src/boletin11/Intervalo.java
@@ -14,17 +14,23 @@ import javax.swing.JOptionPane;
  */
 public class Intervalo {
 
+    private String fraseAescribir = "A documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática.";
+    private String fraseQueEscribo;
     private long diferencia;
     private Date intervaloInicio;
     private Date intervaloFin;
 
     public void intervalo() {
         intervaloInicio = new Date();
-        JOptionPane.showInputDialog("Escribe a siguiente frase" + "\n A documentaciÃ³n Ã© todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicaciÃ³n informÃ¡tica.");
-        intervaloFin = new Date();
-        diferencia = intervaloFin.getTime() - intervaloInicio.getTime();
-        diferencia = (diferencia / 1000);
-        JOptionPane.showMessageDialog(null, "Tardaches " + diferencia + " segundos en escribir a frase");
+        fraseQueEscribo = JOptionPane.showInputDialog("Escribe a siguiente frase" + "\nA documentación é todo aquel conxunto de manuais impresos ou en formato dixital que explique unha aplicación informática.");
+        if (fraseAescribir.equals(fraseQueEscribo)) {
+            intervaloFin = new Date();
+            diferencia = intervaloFin.getTime() - intervaloInicio.getTime();
+            diferencia = (diferencia / 1000);
+            JOptionPane.showMessageDialog(null, "Tardaches " + diferencia + " segundos en escribir a frase");
+        } else {
+            JOptionPane.showMessageDialog(null, "La frase está mal");
+        }
     }
 
 }
